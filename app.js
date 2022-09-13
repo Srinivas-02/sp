2bc08b6194abf33fd523aad859424a2b875019bc
Srinivas-02 <88418542+Srinivas-02@users.noreply.github.com>
2022-09-13 22:05:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-var http = require('http');  
-//create a server object:  
-  
-http.createServer(function (req, res) {  
-    res.write('Hello World!'); //write a response to the client  
-    res.end(); //end the response  
-}).listen(5000); //the server object listens on port 8080   
-  
-// Console will print the message  
-console.log('Server running at 5000');
+var http = require("http");
+
+http.createServer(function (request, response) {
+   // Send the HTTP header 
+   // HTTP Status: 200 : OK
+   // Content Type: text/plain
+   response.writeHead(200, {'Content-Type': 'text/plain'});
+   
+   // Send the response body as "Hello World"
+   response.end('Hello World\n');
+}).listen(8081);
+
+// Console will print the message
+console.log('Server running at http://127.0.0.1:8081/');
